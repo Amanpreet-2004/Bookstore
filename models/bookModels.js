e57f2f68ee158a86180ad80e0af8e4d0525a5193
Amanpreet-2004 <kaurcsamanpreet@gmail.com>
2024-10-01 11:10:22 +0530

    bookstore

---
+++ models/bookModels.js
@@ -0,0 +1,9 @@
+const mongoose=require("mongoose");
+
+const bookschema=new
+mongoose.Schema({
+    title:String,
+    name:String,
+    author:String,
+});
+module.exports=mongoose.model("Book",bookschema);
